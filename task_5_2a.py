ed547e2ca050eddf3b85cb17d2e5c65826a943a8
ustasone <ustasone@gmail.com>
2020-01-14 17:51:44 +0300

    task 5_2a started

--- task_5_2a.py
+++ task_5_2a.py
@@ -27,11 +27,24 @@ mask = temp[-1]
 dec_mask=int(mask)
 last = 32-dec_mask
 octets = numbers.split(".")
+bit_address = '{:08b}{:08b}{:08b}{:08b}'.format(int(octets[0]), int(octets[1]), int(octets[2]), int(octets[3]))
+
+
+
+#print(bit_address)
+
+net_addr = bit_address[0:dec_mask]
+print('bit_addt', net_addr)
+full_net_addr = net_addr+'0'*last
+print('new_full_addr', full_net_addr)
+
 ip_template = '''
 Network:
 {:8} {:8} {:8} {:8}
 {:08b} {:08b} {:08b} {:08b}
 '''
+
+
 print(ip_template.format(octets[0], octets[1], octets[2], octets[3], int(octets[0]), int(octets[1]), int(octets[2]), int(octets[3])))
 
 netmask_template = '''
@@ -46,4 +59,5 @@ bitlast = "0"*last
 #print(bitlast)
 fullbitmask = bitmask+bitlast
 #print(fullbitmask)
+
 print( netmask_template.format(dec_mask, fullbitmask[0:8], fullbitmask[8:16], fullbitmask[16:24], fullbitmask[24:32], int(fullbitmask[0:8], 2), int(fullbitmask[8:16], 2), int(fullbitmask[16:24], 2), int(fullbitmask[24:32], 2)))
