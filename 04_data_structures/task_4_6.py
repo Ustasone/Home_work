dc9304ae2d634e4d986cb2aca4730ac89ecfde5e
ustasone <ustasone@gmail.com>
2020-01-23 17:32:46 +0300

    task_4_6.py done

---
+++ 04_data_structures/task_4_6.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+Задание 4.6
+
+Обработать строку ospf_route и вывести информацию на стандартный поток вывода в виде:
+Protocol:              OSPF
+Prefix:                10.0.24.0/24
+AD/Metric:             110/41
+Next-Hop:              10.0.13.3
+Last update:           3d18h
+Outbound Interface:    FastEthernet0/0
+
+Ограничение: Все задания надо выполнять используя только пройденные темы.
+
+'''
+
+ospf_route = 'O        10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0'
+
+template = '''
+Protocol:              {}
+Prefix:                {}
+AD/Metric:             {}
+Next-Hop:              {}
+Last update:           {}
+Outbound Interface:    {}
+'''
+new_ospf = ospf_route.replace(',','')
+new_ospf = new_ospf.replace('[','')
+new_ospf = new_ospf.replace(']','')
+new_ospf = new_ospf.replace('O','OSPF')
+print(new_ospf)
+ospf = new_ospf.split()
+print(ospf)
+
+print(template.format(ospf[0],ospf[1],ospf[2],ospf[3],ospf[4],ospf[5]))
