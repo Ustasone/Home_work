8a7441be89f10bc99dbafb5356a79863fd7d6983
ustasone <ustasone@gmail.com>
2020-01-29 18:21:28 +0300

    task_7_2c.py done

--- 07_files/task_7_2c.py
+++ 07_files/task_7_2c.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+from sys import argv
 # -*- coding: utf-8 -*-
 '''
 Задание 7.2c
@@ -15,5 +17,33 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
+file_read = argv[1]
+file_write = argv[2]
 
 ignore = ['duplex', 'alias', 'Current configuration']
+
+cfg_lines = []
+
+with open( file_read, 'r') as f:
+    for string in f:
+            for i in ignore:
+                if string.find(i) >= 0:
+                    string = '#'
+                    break
+                else:
+                    pass
+            if string.startswith('#'):
+                pass
+            else:
+                string = string.rstrip('\n')
+                cfg_lines.append(string)
+#print(cfg_lines)
+cfg_lines2 = []
+for line in cfg_lines:
+    cfg_lines2.append( line + '\n' )
+file = open( file_write, 'w')
+file.writelines(cfg_lines2)
+file.close()
+
+
+
