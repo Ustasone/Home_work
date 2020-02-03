782a0632c53c503a47e4b8c74ce72da7904a5f8d
ustasone <ustasone@gmail.com>
2020-02-03 15:32:29 +0300

    task_7_3b.py done

--- 07_files/task_7_3b.py
+++ 07_files/task_7_3b.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 '''
 Задание 7.3b
@@ -11,3 +12,61 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 '''
+vlan = input('Enter VLAN number:')
+ignore = ['sw1', 'Mac', '---', 'Vlan']
+new_dict = {}
+j = 0
+#print(new_dict)
+with open('CAM_table.txt', 'r') as f:
+    for string in f:
+        #j = j + 1
+        for i in ignore:
+            if string.find(i) >= 0:
+                string = '#'
+                break
+            else:
+                pass
+        if string.startswith('#'):
+            pass
+        else:
+            string = string.rstrip('\n')
+            string = (string.replace('  DYNAMIC   ',''))
+            new_list = string.split()
+            if len(new_list) > 0:
+                new_dict.update({j: new_list})
+                j = j + 1
+print(new_dict)
+print('--------------------------')
+P = 0
+#k = 0
+#n = 0
+temp = {}
+for k in range(j):
+    for n in range(j-k-2):
+        #if new_dict[n][0] <= new_dict[n + 1][0]:
+            #pass
+            #print('OK')
+            #P = 1
+        #else:
+        if new_dict[n][0] >= new_dict[n + 1][0]:
+            temp.update({'0': new_dict[n]})
+            new_dict.update({n: new_dict[n + 1]})
+            new_dict.update({n + 1: temp['0']})
+            #print(temp)
+            P = 1
+            #print('NOK')
+        else:
+            pass
+    if P == 0:
+        break
+    else:
+        pass
+output_template = '''{:4}     {:14}     {:5}'''
+for q in range(j):
+    if vlan == new_dict[q][0]:
+        temp_string = output_template.format(new_dict[q][0],new_dict[q][1],new_dict[q][2])
+        print(temp_string.rstrip())
+    else:
+        pass
+
+
